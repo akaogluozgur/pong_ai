bc5f43f0210f244af64db8a8c2905c0bf589303d
enes.yalcin <enes.yalcin@algosis.com.tr>
2019-05-28 01:08:41 +0300

    works

---
+++ player.py
@@ -0,0 +1,64 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+import numpy as np
+import operator
+
+MUT_POS = 0.05
+
+
+class Player:
+    def __init__(self):
+        self.score = 0
+        self.brain = Sequential()
+        self.brain.add(Dense(12, input_dim=34, activation='relu'))
+        self.brain.add(Dense(4))
+        self.brain.add(Dense(2, activation="sigmoid"))
+        self.brain.compile(optimizer="adam", loss="mse")
+        
+    def move(self, inputs):
+        inputs = np.array(inputs)
+        prediction = self.brain.predict(inputs.reshape(1,34))[0]
+        output = []
+        if prediction[0] < 0.33:
+            output.append(-1) # move down
+        elif prediction[0] < 0.66:
+            output.append(0) # do not move
+        else:
+            output.append(1) # move up
+        
+        if prediction[1] < 0.5:
+            output.append(0) # do not shoot
+        else:
+            output.append(1) # shoot
+        return output
+
+def breed(p_one, p_two):
+    p_child = Player()
+    p_one_weights = p_one.brain.get_weights()
+    p_two_weights = p_two.brain.get_weights()
+    
+    child_weights = []
+    for dad, mum in zip(p_one_weights, p_two_weights):
+        curr_weights = []
+        for dad_w, mum_w in zip(dad, mum):
+            curr_w = 0
+            if(not isinstance(dad_w, np.float32)):
+                curr_w = []
+                for dad_w_w, mum_w_w in zip(dad_w, mum_w):
+                    cross_ratio = np.random.uniform()
+                    curr_w.append(dad_w_w*cross_ratio + mum_w_w*(1-cross_ratio))
+            else:
+                cross_ratio = np.random.uniform()
+                curr_w = dad_w*cross_ratio + mum_w*(1-cross_ratio)
+            curr_weights.append(curr_w) 
+        child_weights.append(curr_weights)
+    
+    p_child.brain.set_weights(child_weights)
+    return p_child
+
+def mutate(weight):
+    if np.random.uniform() <= MUT_POS:
+        mut_ratio = np.random.uniform(-0.5, 0.5)
+        weight += weight * mut_ratio
+    return weight
--- pong.py
+++ pong.py
@@ -3,13 +3,37 @@ import turtle
 import os
 import random
 import time
+from player import Player
+from player import breed
+from itertools import combinations
+import operator
+import numpy as np
+import json
+from datetime import datetime
+
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+wn.listen()
 
 total_hp = 3
+generation_num = 0
+population_num = 10 # even number is required
+children_number = 3
+random_number = int(population_num/2) - children_number
+max_round = 1000
+
+cur_pop = []
+for i in range(population_num):
+    cur_pop.append(Player())
+
+combos = list(combinations(range(population_num),2))
+
+p_a, p_b = None, None
+
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -39,7 +63,7 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Remaining HP A: {}  Remaining HP B: {}".format(hp_a, hp_b), align="center", font=("Courier", 24, "normal"))
+
 
 # Bullet list
 bullet_list_a = []
@@ -49,9 +73,21 @@ ballspeed = 10
 last_move_a = ballspeed,0
 last_move_b = -1 * ballspeed,0
 
-
+clipsize = 3
 
 # Functions
+def save_pop_nn():
+    output_dict = {}
+    for ix, p in enumerate(cur_pop):
+        output_dict[str(ix)] = p.brain.get_weights()
+    file_name = "nn_weights" + str(datetime.now()) + ".json"
+    f = open(file_name, "w")
+    json.dump(output_dict, f)
+    f.flush()
+    f.close()
+
+wn.onkeypress(save_pop_n, "s")
+
 def paddle_a_up():
     global last_move_a
     y = paddle_a.ycor()
@@ -112,6 +148,7 @@ def paddle_b_shoot():
 
 def reset_game():
     global paddle_a, paddle_b, pen, bullet_list_a, bullet_list_b, last_move_a, last_move_b, hp_a, hp_b, total_hp, wn
+
     
     paddle_a.setx(-350)
     paddle_a.sety(0)
@@ -122,7 +159,7 @@ def reset_game():
     hp_a = total_hp
     hp_b = total_hp
     pen.clear()
-    pen.write("Remaining HP A: {}  Remaining HP B: {}".format(hp_a, hp_b), align="center", font=("Courier", 24, "normal"))
+    pen.write("Rem HP A: {}  Rem HP B: {} --GN:{}".format(hp_a, hp_b, generation_num), align="center", font=("Courier", 24, "normal"))
 
     for bullet in bullet_list_a + bullet_list_b:
         bullet.hideturtle()
@@ -134,94 +171,215 @@ def reset_game():
 
 # Main game loop
 def make_move_a():
-    up_or_down = random.randint(0,1)
-    if up_or_down == 0:
-        paddle_a_up()
-    else:
-        paddle_a_down()
+    global last_move_a
+    inputs = [  hp_a, 
+                hp_b, 
+                clipsize - len(bullet_list_a),
+                clipsize - len(bullet_list_b),
+                paddle_a.ycor() + 50,
+                paddle_a.ycor() - 50,
+                paddle_b.ycor() + 50,
+                paddle_b.ycor() - 50,
+                last_move_a[1],
+                last_move_b[1],
+                 ]
+
+    for index in range(clipsize):
+        try:
+            inputs.append(abs(paddle_a.xcor() - bullet_list_a[index].xcor()))
+            inputs.append(bullet_list_a[index].ycor())
+            inputs.append(abs(bullet_list_a[index].dx))
+            inputs.append(bullet_list_a[index].dy)
+        except:
+            inputs.append(1000)
+            inputs.append(1000)
+            inputs.append(0)
+            inputs.append(0)
+
+    for index in range(clipsize):
+        try:
+            inputs.append(abs(paddle_b.xcor() - bullet_list_b[index].xcor()))
+            inputs.append(bullet_list_b[index].ycor())
+            inputs.append(abs(bullet_list_b[index].dx))
+            inputs.append(bullet_list_b[index].dy)
+        except:
+            inputs.append(1000)
+            inputs.append(1000)
+            inputs.append(0)
+            inputs.append(0)
+
+    up_or_down, shoot = p_a.move(inputs)
     
-    shoot = random.randint(0,50)
-    if shoot == 0:
+    if up_or_down == -1:
+        paddle_a_down()
+    elif up_or_down == 0:
+        last_move_a = ballspeed,0
+    else:
+        paddle_a_up()
+   
+    if shoot == 1:
         paddle_a_shoot()
 
 def make_move_b():
-    up_or_down = random.randint(0,1)
-    if up_or_down == 0:
-        paddle_b_up()
-    else:
+    global last_move_b
+    inputs = [  hp_b, 
+                hp_a, 
+                clipsize - len(bullet_list_b),
+                clipsize - len(bullet_list_a),
+                paddle_b.ycor() + 50,
+                paddle_b.ycor() - 50,
+                paddle_a.ycor() + 50,
+                paddle_a.ycor() - 50,
+                last_move_b[1],
+                last_move_a[1],
+                 ]
+
+    for index in range(clipsize):
+        try:
+            inputs.append(abs(paddle_b.xcor() - bullet_list_b[index].xcor()))
+            inputs.append(bullet_list_b[index].ycor())
+            inputs.append(abs(bullet_list_b[index].dx))
+            inputs.append(bullet_list_b[index].dy)
+        except:
+            inputs.append(1000)
+            inputs.append(1000)
+            inputs.append(0)
+            inputs.append(0)
+
+    for index in range(clipsize):
+        try:
+            inputs.append(abs(paddle_a.xcor() - bullet_list_a[index].xcor()))
+            inputs.append(bullet_list_a[index].ycor())
+            inputs.append(abs(bullet_list_a[index].dx))
+            inputs.append(bullet_list_a[index].dy)
+        except:
+            inputs.append(1000)
+            inputs.append(1000)
+            inputs.append(0)
+            inputs.append(0)
+
+    up_or_down, shoot = p_b.move(inputs)
+    
+    if up_or_down == -1:
         paddle_b_down()
-    
-    shoot = random.randint(0,50)
-    if shoot == 0:
+    elif up_or_down == 0:
+        last_move_b = -1 * ballspeed,0
+    else:
+        paddle_b_up()
+   
+    if shoot == 1:
         paddle_b_shoot()
-while True:
-    wn.update()
-    
-    move_func = {
-        "0": make_move_a,
-        "1":make_move_b
-    }
-
-    turn = random.randint(0, 1)
-    move_func[str(turn)]()
-    move_func[str(1 - turn)]()
-
-    # Move the ball
-    bullets = bullet_list_a + bullet_list_b
-    for ball in bullets:
-        ball.setx(ball.xcor() + ball.dx)
-        ball.sety(ball.ycor() + ball.dy)
 
 
-    # Border checking
+while True:    
+    generation_num += 1
+    pen.write("Rem HP A: {}  Rem HP B: {} --GN: {} Round {}/{}".format(hp_a, hp_b, generation_num,0,len(combos)), align="center", font=("Courier", 24, "normal"))
+    # Round Starts
+    for ix, combo in enumerate(combos):
+        pen.clear()
+        pen.write("Rem HP A: {}  Rem HP B: {} --GN: {} Round {}/{}".format(hp_a, hp_b, generation_num, ix, len(combos)), align="center", font=("Courier", 24, "normal"))
+        p_a = cur_pop[combo[0]]
+        p_b = cur_pop[combo[1]]
 
-    # Top and bottom
-    for ball in bullets:
-        if ball.ycor() > 290:
-            ball.sety(290)
-            ball.dy *= -1
-        
-    
-        elif ball.ycor() < -290:
-            ball.sety(-290)
-            ball.dy *= -1
-        
-
-    # Left and right
-    for ball in bullet_list_a.copy():
-        if ball.xcor() > 350:
-            ball.hideturtle()
-            bullet_list_a.remove(ball)
-
-
-    for ball in bullet_list_b.copy():
-        if ball.xcor() < -350:
-            ball.hideturtle()
-            bullet_list_b.remove(ball)
-
-
-    # Paddle and ball collisions
-    for ball in bullet_list_b.copy():
-        if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
-            hp_a -= 1
-            if hp_a > 0:
-                pen.clear()
-                pen.write("Remaining HP A: {}  Remaining HP B: {}".format(hp_a, hp_b), align="center", font=("Courier", 24, "normal"))
-                ball.hideturtle()
-                bullet_list_b.remove(ball)
-            else:
+        counter = max_round
+        is_finished = False
+        while not is_finished:
+            counter -=1
+            if counter == 0:
+                p_a.score += hp_a - hp_b
+                p_b.score += hp_b - hp_a
                 reset_game()
-    for ball in bullet_list_a.copy():
-        if ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
-            hp_b -= 1
-            if hp_b > 0:
-                pen.clear()
-                pen.write("Remaining HP A: {}  Remaining HP B: {}".format(hp_a, hp_b), align="center", font=("Courier", 24, "normal"))
-                ball.hideturtle()
-                bullet_list_a.remove(ball)
-            else:
-                reset_game()
-        
+                is_finished = True
+                continue
+
+            wn.update()
+            
+            move_func = {
+                "0": make_move_a,
+                "1":make_move_b
+            }
+
+            turn = random.randint(0, 1)
+            move_func[str(turn)]()
+            move_func[str(1 - turn)]()
+
+            # Move the ball
+            bullets = bullet_list_a + bullet_list_b
+            for ball in bullets:
+                ball.setx(ball.xcor() + ball.dx)
+                ball.sety(ball.ycor() + ball.dy)
+
+
+            # Border checking
+
+            # Top and bottom
+            for ball in bullets:
+                if ball.ycor() > 290:
+                    ball.sety(290)
+                    ball.dy *= -1
+                
+            
+                elif ball.ycor() < -290:
+                    ball.sety(-290)
+                    ball.dy *= -1
+                
+
+            # Left and right
+            for ball in bullet_list_a.copy():
+                if ball.xcor() > 350:
+                    ball.hideturtle()
+                    bullet_list_a.remove(ball)
+
+
+            for ball in bullet_list_b.copy():
+                if ball.xcor() < -350:
+                    ball.hideturtle()
+                    bullet_list_b.remove(ball)
+
+
+            # Paddle and ball collisions
+            for ball in bullet_list_b.copy():
+                if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+                    hp_a -= 1
+                    if hp_a > 0:
+                        pen.clear()
+                        pen.write("Rem HP A: {}  Rem HP B: {} --GN: {} Round {}/{}".format(hp_a, hp_b, generation_num, ix, len(combos)), align="center", font=("Courier", 24, "normal"))
+                        ball.hideturtle()
+                        bullet_list_b.remove(ball)
+                    else:
+                        p_a.score += hp_a - hp_b
+                        p_b.score += hp_b - hp_a
+                        reset_game()
+                        is_finished = True
+            for ball in bullet_list_a.copy():
+                if ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+                    hp_b -= 1
+                    if hp_b > 0:
+                        pen.clear()
+                        pen.write("Rem HP A: {}  Rem HP B: {} --GN: {} Round {}/{}".format(hp_a, hp_b, generation_num, ix, len(combos)), align="center", font=("Courier", 24, "normal"))
+                        ball.hideturtle()
+                        bullet_list_a.remove(ball)
+                    else:
+                        p_a.score += hp_a - hp_b
+                        p_b.score += hp_b - hp_a
+                        reset_game()
+                        is_finished = True
+    
+    cur_pop.sort(key=operator.attrgetter('score'), reverse=True)
+
+    cur_pop = cur_pop[: int(len(cur_pop)/2)]
+    cur_pop_cp = cur_pop.copy()
+    for i in range(children_number):
+        father, mother = np.random.choice(cur_pop_cp, size = 2)
+        child = breed(father, mother)
+        cur_pop.append(child)
+
+    for i in range(random_number):
+        cur_pop.append(Player())
+    
+    for ix in range(len(cur_pop)):
+        cur_pop[ix].score = 0
+
     
 
 
